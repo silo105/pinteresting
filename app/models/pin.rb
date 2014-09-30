1994acdb7ae41ddf8ec8217f0529b297cb433c11
silo105 <silo105@hushmail.com>
2014-09-29 23:36:44 -0400

    Added Pagination to pins index

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,11 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
   	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+  	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  	#validates_attachment_content_type :data, :content_type => /\Aimage/
+  	#validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  	#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  	#Paperclip.options[:content_type_mappings] = { jpeg: 'image/jpeg', jpg: 'image/jpeg' }
+	  #validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 	  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,9 @@
-#require 'paperclip/media_type_spoof_detector'
-#module Paperclip
-#  class MediaTypeSpoofDetector
-#    def spoofed?
-#      false
-#    end
-#  end
-#end
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
+#Paperclip.options[:content_type_mappings] = { jpeg: 'image/jpeg', jpg: 'image/jpeg' }
