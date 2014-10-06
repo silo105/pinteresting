29cdbe1fc79c56fad5c2911d0502b752fc00157c
silo105 <silo105@hushmail.com>
2014-10-05 23:43:38 -0400

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,8 @@ class Pin < ActiveRecord::Base
 
   	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  	validates :desription, presence: true
+  	validates :image, presence: true
   	#validates_attachment_content_type :data, :content_type => /\Aimage/
   	#validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
   	#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
